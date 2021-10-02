416070157a5304cad092ba783563fe3920d55b97
Mert Koprucu <mrtkprc@gmail.com>
2021-10-02 14:23:48 +0300

    Configure signalr for Startup.cs

--- Chatty.Api/Startup.cs
+++ Chatty.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chatty.Api.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace Chatty.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Chatty.Api", Version = "v1" });
             });
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,7 @@ namespace Chatty.Api
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<ChatHub>("/hubs/chat");
                 endpoints.MapControllers();
             });
         }
