403ad01894bbc9fceb20f83d6b65fe4980079658
Mert Koprucu <mrtkprc@gmail.com>
2021-10-02 14:22:23 +0300

    Create chat message structure.

---
+++ Chatty.Api/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chatty.Api.Hubs.Clients;
+using Chatty.Api.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chatty.Api.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
