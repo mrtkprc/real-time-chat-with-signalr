e84d28d67e1f506a355775ae77c4cf9926552f6f
Mert Koprucu <mrtkprc@gmail.com>
2021-10-02 15:10:26 +0300

    Send message from outside the hub

---
+++ Chatty.Api/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using Chatty.Api.Hubs;
+using Chatty.Api.Hubs.Clients;
+using Chatty.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
+
+        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        public async Task Post(ChatMessage message)
+        {
+            // run some logic...
+
+            await _chatHub.Clients.All.ReceiveMessage(message);
+        }
+    }
+}
--- Chatty.Api/Hubs/ChatHub.cs
+++ Chatty.Api/Hubs/ChatHub.cs
@@ -10,9 +10,5 @@ namespace Chatty.Api.Hubs
 {
     public class ChatHub : Hub<IChatClient>
     {
-        public async Task SendMessage(ChatMessage message)
-        {
-            await Clients.All.ReceiveMessage(message);
-        }
     }
 }
